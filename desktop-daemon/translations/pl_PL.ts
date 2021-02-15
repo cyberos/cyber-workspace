244493f02d2000ee332bd087fa54097ad3b73555
omame <me@omame.tech>
2021-02-16 00:07:36 +0100

    Add Polish translations

---
+++ desktop-daemon/translations/pl_PL.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>Desktop</name>
+    <message>
+        <location filename="../Desktop.qml" line="22"/>
+        <source>Change wallpaper</source>
+        <translation>Zmień tapetę</translation>
+    </message>
+</context>
+<context>
+    <name>DesktopView</name>
+    <message>
+        <location filename="../desktopview.cpp" line="12"/>
+        <source>Desktop</source>
+        <translation>Pulpit</translation>
+    </message>
+</context>
+</TS>
