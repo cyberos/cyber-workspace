831dfe417964a9a18905e96ff8ea658bc72f7875
omame <me@omame.tech>
2021-02-19 10:14:01 +0000

    Translated using Weblate (English (United States))
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/en_US/

--- desktop-daemon/translations/en_US.ts
+++ desktop-daemon/translations/en_US.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>Desktop</name>
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation>Change wallpaper</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Desktop</translation>
     </message>
 </context>
 </TS>
