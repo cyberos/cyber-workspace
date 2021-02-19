33de140c395fba7d5f99170230e3d9939092925e
omame <me@omame.tech>
2021-02-19 10:32:40 +0000

    Translated using Weblate (English (United States))
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/en_US/

--- shutdown-ui/translations/en_US.ts
+++ shutdown-ui/translations/en_US.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>main</name>
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Shutdown</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Reboot</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Logout</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Suspend</translation>
     </message>
 </context>
 </TS>
