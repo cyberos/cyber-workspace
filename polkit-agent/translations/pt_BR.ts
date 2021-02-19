8b0ecf911c05d5337a5fca182b790e5bee67428f
Anonymous <noreply@weblate.org>
2021-02-19 11:28:16 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/pt_BR/

--- polkit-agent/translations/pt_BR.ts
+++ polkit-agent/translations/pt_BR.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Senha</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
@@ -16,7 +16,7 @@
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cancelar</translation>
     </message>
 </context>
 </TS>
