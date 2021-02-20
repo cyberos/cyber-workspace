3192849efb0382ae95736de31f19188b59bf894a
Guilherme Félix <guilhermefelix592@gmail.com>
2021-02-20 13:30:31 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/pt_BR/

--- desktop-daemon/translations/pt_BR.ts
+++ desktop-daemon/translations/pt_BR.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation>Alterar plano de fundo</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Área de trabalho</translation>
     </message>
 </context>
 </TS>
