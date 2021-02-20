b4bc1b83ad1a8a89e8ea3020c7e4715a2be944b1
Adolfo Jayme Barrientos <fitojb@ubuntu.com>
2021-02-20 10:54:35 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/es/

--- desktop-daemon/translations/es_ES.ts
+++ desktop-daemon/translations/es_ES.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambiar fondo de pantalla</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation>Escritorio</translation>
     </message>
 </context>
 </TS>
