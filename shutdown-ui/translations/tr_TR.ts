cb0e5c03024abf5031173cf54759584b4f44990c
Anonymous <noreply@weblate.org>
2021-02-27 14:40:42 +0000

    Translated using Weblate (Turkish)
    Currently translated at 50.0% (2 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/tr/

--- shutdown-ui/translations/tr_TR.ts
+++ shutdown-ui/translations/tr_TR.ts
@@ -16,7 +16,7 @@
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Çıkış yap</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
