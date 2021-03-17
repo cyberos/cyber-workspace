d3a5d6da0967888a2037f0c16f1dc7317038ee93
matriz <wboy111@outlook.com>
2021-03-17 05:26:43 +0000

    Translated using Weblate (Chinese (Traditional, Hong Kong))
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/zh_Hant_HK/

--- shutdown-ui/translations/zh_Hant_HK.ts
+++ shutdown-ui/translations/zh_Hant_HK.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>熄機</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>重新開機</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>登出</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>休眠</translation>
     </message>
 </context>
 </TS>
