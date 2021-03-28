1efeb9ef733600038bb133c9b7d0f09bf93e0060
Anonymous <noreply@weblate.org>
2021-03-28 13:03:04 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 0.0% (0 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/es_MX/

--- desktop-daemon/translations/es_MX.ts
+++ desktop-daemon/translations/es_MX.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cambiar el fondo de pantalla</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Escritorio</translation>
     </message>
 </context>
 </TS>
