df53060db47e06bfc451d1cd436f223818be55ea
Anonymous <noreply@weblate.org>
2021-03-29 13:05:04 +0000

    Translated using Weblate (Sinhala)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/si/

--- polkit-agent/translations/si_LK.ts
+++ polkit-agent/translations/si_LK.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">රහස් පදය</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">සිදු විය</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">අවලංගු කරන්න</translation>
     </message>
 </context>
 </TS>
