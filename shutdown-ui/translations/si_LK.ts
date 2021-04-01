2079b3c5bea81cb1812745d823c3001b8d58a835
Anonymous <noreply@weblate.org>
2021-04-01 13:08:49 +0000

    Translated using Weblate (Sinhala)
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/si/

--- shutdown-ui/translations/si_LK.ts
+++ shutdown-ui/translations/si_LK.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">වසා දමන්න</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">නැවත ආරම්භ කරන්න</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ඉවත් වන්න</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">අත්හිටුවන්න</translation>
     </message>
 </context>
 </TS>
