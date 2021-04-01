4cbaf858974dbffd6a59fd7f84748d210aae0688
Anonymous <noreply@weblate.org>
2021-04-01 13:08:54 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/es_MX/

--- shutdown-ui/translations/es_MX.ts
+++ shutdown-ui/translations/es_MX.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Apagado</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Reiniciar</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cerrar la sesión</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Suspender</translation>
     </message>
 </context>
 </TS>
