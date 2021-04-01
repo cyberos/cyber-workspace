99862190f06a924f2ec4a508c4b7d84b40602e62
Anonymous <noreply@weblate.org>
2021-04-01 13:08:58 +0000

    Translated using Weblate (Italian)
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/it/

--- shutdown-ui/translations/it_IT.ts
+++ shutdown-ui/translations/it_IT.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Spegnimento</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Riavvio</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
@@ -21,7 +21,7 @@
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Sospendere</translation>
     </message>
 </context>
 </TS>
