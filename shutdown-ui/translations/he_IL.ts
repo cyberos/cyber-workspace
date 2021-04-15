fe5cc85c69a46fe3a6172951c51d2a94facfea99
Anonymous <noreply@weblate.org>
2021-04-15 10:59:45 +0000

    Translated using Weblate (Hebrew (Israel))
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/he_IL/

--- shutdown-ui/translations/he_IL.ts
+++ shutdown-ui/translations/he_IL.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">לכבות</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">לְאַתחֵל</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">להתנתק</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">לְהַשְׁעוֹת</translation>
     </message>
 </context>
 </TS>
