5046b3455285a48e45636c14b98cb10b9a0295a0
Aikoyori <gurockherobrine123@gmail.com>
2021-04-16 09:27:38 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/th/

--- shutdown-ui/translations/th_TH.ts
+++ shutdown-ui/translations/th_TH.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished">ปิดตัวลง</translation>
+        <translation>ปิดเครื่อง</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished">รีบูต</translation>
+        <translation>รีบูต</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished">ออกจากระบบ</translation>
+        <translation>ออกจากระบบ</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished">ระงับ</translation>
+        <translation>พักการใช้งาน</translation>
     </message>
 </context>
 </TS>
