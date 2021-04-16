6a6342a3621a75e310aa2b39ce04db691e6d1010
Anonymous <noreply@weblate.org>
2021-04-16 06:12:13 +0000

    Translated using Weblate (Thai)
    Currently translated at 0.0% (0 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/th/

--- desktop-daemon/translations/th_TH.ts
+++ desktop-daemon/translations/th_TH.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">เปลี่ยนวอลเปเปอร์</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">เดสก์ทอป</translation>
     </message>
 </context>
 </TS>
