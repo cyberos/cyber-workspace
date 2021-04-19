5916de43af1831b1e7fe7bf27bc1053c78a44ca0
Anonymous <noreply@weblate.org>
2021-04-19 15:06:18 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/uk/

--- polkit-agent/translations/uk_UA.ts
+++ polkit-agent/translations/uk_UA.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Пароль</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Готово</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Скасувати</translation>
     </message>
 </context>
 </TS>
