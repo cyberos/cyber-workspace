69acdf2ce51d4d5383caefd1162c36832d0f46cd
Максим Грабовий <dechioyo@gmail.com>
2021-04-19 15:11:07 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/uk/

--- shutdown-ui/translations/uk_UA.ts
+++ shutdown-ui/translations/uk_UA.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished">Закрити</translation>
+        <translation>Вимкнути ПК</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished">Перезавантажте</translation>
+        <translation>Перезавантажити</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished">Вийти</translation>
+        <translation>Вийти</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished">Призупинити</translation>
+        <translation>Режим сну</translation>
     </message>
 </context>
 </TS>
