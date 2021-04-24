43c00ad62dfdf7ddcdc7aff1520fdcc3f5242fcf
Weblate <noreply@weblate.org>
2021-04-24 16:16:45 +0200

    Added translation using Weblate (Persian)

---
+++ desktop-daemon/translations/fa_IR.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fa" sourcelanguage="de_DE">
+<context>
+    <name>Desktop</name>
+    <message>
+        <location filename="../Desktop.qml" line="22"/>
+        <source>Change wallpaper</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DesktopView</name>
+    <message>
+        <location filename="../desktopview.cpp" line="12"/>
+        <source>Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
