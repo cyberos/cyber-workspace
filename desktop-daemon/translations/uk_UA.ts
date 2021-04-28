f57bad01b221d94672555dc7a6e357cbb16ee20b
Максим Грабовий <dechioyo@gmail.com>
2021-04-28 08:22:30 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/uk/

--- desktop-daemon/translations/uk_UA.ts
+++ desktop-daemon/translations/uk_UA.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished">Змінити шпалери</translation>
+        <translation>Змінити шпалери</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished">Робочий стіл</translation>
+        <translation>Стільниця</translation>
     </message>
 </context>
 </TS>
