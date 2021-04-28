240e7050a10f07d52d461900c967ff189ddbf0d5
Anonymous <noreply@weblate.org>
2021-04-28 15:50:38 +0000

    Translated using Weblate (Greek)
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/el/

--- shutdown-ui/translations/el_GR.ts
+++ shutdown-ui/translations/el_GR.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Κλείσιμο</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Επανεκκίνηση</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Αποσύνδεση</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Αναστολή</translation>
     </message>
 </context>
 </TS>
