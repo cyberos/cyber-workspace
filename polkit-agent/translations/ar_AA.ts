e0a0c804b4c728d70c8ff5a18e19c671e3bae554
Anonymous <noreply@weblate.org>
2021-04-30 23:41:21 +0000

    Translated using Weblate (Arabic)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/ar/

--- polkit-agent/translations/ar_AA.ts
+++ polkit-agent/translations/ar_AA.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">الرقم السرى</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">منتهي</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">إلغاء</translation>
     </message>
 </context>
 </TS>
