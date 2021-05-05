337c078d4e81de3b3bb40a8546236d3f3578d135
Anonymous <noreply@weblate.org>
2021-05-05 12:31:08 +0000

    Translated using Weblate (Hindi)
    Currently translated at 0.0% (0 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/hi/

--- desktop-daemon/translations/hi_IN.ts
+++ desktop-daemon/translations/hi_IN.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">वॉलपेपर बदल दो</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">डेस्कटॉप</translation>
     </message>
 </context>
 </TS>
