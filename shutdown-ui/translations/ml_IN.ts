344cff3de901a1a3751107a8647681ab8b3ad48e
Anonymous <noreply@weblate.org>
2021-05-05 07:35:48 +0000

    Translated using Weblate (Malayalam)
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/ml/

--- shutdown-ui/translations/ml_IN.ts
+++ shutdown-ui/translations/ml_IN.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ഷട്ട് ഡൗൺ</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">റീബൂട്ട് ചെയ്യുക</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ലോഗ് .ട്ട് ചെയ്യുക</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">സസ്പെൻഡ് ചെയ്യുക</translation>
     </message>
 </context>
 </TS>
