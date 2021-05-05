e413162db8210c0f03b59bce0ffaaf0078f824d8
Anonymous <noreply@weblate.org>
2021-05-05 12:31:13 +0000

    Translated using Weblate (Hindi)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/hi/

--- polkit-agent/translations/hi_IN.ts
+++ polkit-agent/translations/hi_IN.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">कुंजिका</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">किया हुआ</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">रद्द करना</translation>
     </message>
 </context>
 </TS>
