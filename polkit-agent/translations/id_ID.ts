7da968709c7230869e1869d86c32ec82f7f22410
Anonymous <noreply@weblate.org>
2021-05-06 02:34:04 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/id/

--- polkit-agent/translations/id_ID.ts
+++ polkit-agent/translations/id_ID.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Kata Sandi</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Selesai</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Membatalkan</translation>
     </message>
 </context>
 </TS>
