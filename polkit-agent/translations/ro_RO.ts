6f6bb5b2bb88b28bb4fba3db1cf1060f27b78edf
Anonymous <noreply@weblate.org>
2021-05-06 15:54:01 +0000

    Translated using Weblate (Romanian)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/ro/

--- polkit-agent/translations/ro_RO.ts
+++ polkit-agent/translations/ro_RO.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Parolă</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Realizat</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Anulează</translation>
     </message>
 </context>
 </TS>
