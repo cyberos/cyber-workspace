87ad4f051476283b0a1b2302cdd7c4e748a3efe1
Anonymous <noreply@weblate.org>
2021-05-13 08:46:02 +0000

    Translated using Weblate (Estonian)
    Currently translated at 0.0% (0 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/et/

--- desktop-daemon/translations/et_EE.ts
+++ desktop-daemon/translations/et_EE.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Taustapildi muutmine</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Töölauaarvuti</translation>
     </message>
 </context>
 </TS>
