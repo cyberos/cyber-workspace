bbf4ed83ddac19ae59d71792e6f0a1c175e1d2cc
404oops <filip.kujundzic@gmail.com>
2021-05-15 14:15:43 +0000

    Translated using Weblate (Serbian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/sr/

--- shutdown-ui/translations/sr_RS.ts
+++ shutdown-ui/translations/sr_RS.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished">Искључити</translation>
+        <translation>Искључити</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished">Рестарт</translation>
+        <translation>Рестарт</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished">Одјавити се</translation>
+        <translation>Одјавити се</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished">Обустави</translation>
+        <translation>Суспендовати</translation>
     </message>
 </context>
 </TS>
