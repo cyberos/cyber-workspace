4d4b8ec5360627f4d2fb604a50695cb761f2be3f
Anonymous <noreply@weblate.org>
2021-05-15 11:23:54 +0000

    Translated using Weblate (Japanese)
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Workspace/Shutdown UI
    Translate-URL: https://hosted.weblate.org/projects/cyberos/shutdown-ui/ja/

--- shutdown-ui/translations/ja_JP.ts
+++ shutdown-ui/translations/ja_JP.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../main.qml" line="47"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">シャットダウン</translation>
     </message>
     <message>
         <location filename="../main.qml" line="55"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">再起動</translation>
     </message>
     <message>
         <location filename="../main.qml" line="63"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ログアウト</translation>
     </message>
     <message>
         <location filename="../main.qml" line="71"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">サスペンド</translation>
     </message>
 </context>
 </TS>
