8587ef2ed081676af9aa45cedc0ea503c8b5a803
maxius65 <max.dewispelaere@hotmail.com>
2021-05-15 19:45:51 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: CyberOS/Workspace/Polkit Agent
    Translate-URL: https://hosted.weblate.org/projects/cyberos/polkit-agent/nl/

--- polkit-agent/translations/nl_NL.ts
+++ polkit-agent/translations/nl_NL.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../main.qml" line="62"/>
         <source>Password</source>
-        <translation type="unfinished">Wachtwoord</translation>
+        <translation>Wachtwoord</translation>
     </message>
     <message>
         <location filename="../main.qml" line="80"/>
         <source>Done</source>
-        <translation type="unfinished">Gedaan</translation>
+        <translation>klaar</translation>
     </message>
     <message>
         <location filename="../main.qml" line="87"/>
         <source>Cancel</source>
-        <translation type="unfinished">Annuleren</translation>
+        <translation>Annuleren</translation>
     </message>
 </context>
 </TS>
