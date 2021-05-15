9c7337eafacc7ea6c96a92d2d56d816f0541bd7d
maxius65 <max.dewispelaere@hotmail.com>
2021-05-15 19:41:19 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CyberOS/Workspace/Desktop Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/desktop-daemon/nl/

--- desktop-daemon/translations/nl_NL.ts
+++ desktop-daemon/translations/nl_NL.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../Desktop.qml" line="22"/>
         <source>Change wallpaper</source>
-        <translation type="unfinished">Verander achtergrond</translation>
+        <translation>Verander achtergrond</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../desktopview.cpp" line="12"/>
         <source>Desktop</source>
-        <translation type="unfinished">Desktop</translation>
+        <translation>Bureaublad</translation>
     </message>
 </context>
 </TS>
