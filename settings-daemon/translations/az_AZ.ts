1875438f11ddfae1f03065207ada9ef2187aa3ba
Anonymous <noreply@weblate.org>
2021-05-18 22:31:48 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 0.0% (0 of 16 strings)
    
    Translation: CyberOS/Workspace/Settings Daemon
    Translate-URL: https://hosted.weblate.org/projects/cyberos/settings-daemon/az/

--- settings-daemon/translations/az_AZ.ts
+++ settings-daemon/translations/az_AZ.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../battery/battery.cpp" line="268"/>
         <source>d</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">d</translation>
     </message>
     <message>
         <location filename="../battery/battery.cpp" line="274"/>
         <source>h</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">h</translation>
     </message>
     <message>
         <location filename="../battery/battery.cpp" line="281"/>
         <source>m</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">m</translation>
     </message>
     <message>
         <location filename="../battery/battery.cpp" line="290"/>
         <source>until fully charged.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">tam doldurulana qədər.</translation>
     </message>
     <message>
         <location filename="../battery/battery.cpp" line="292"/>
         <source>remaining.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">qalan.</translation>
     </message>
     <message>
         <location filename="../battery/battery.cpp" line="294"/>
         <source>Fully charged.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tamamilə doldurulub.</translation>
     </message>
     <message>
         <location filename="../battery/battery.cpp" line="317"/>
         <source>now</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">İndi</translation>
     </message>
     <message>
         <location filename="../battery/battery.cpp" line="319"/>
         <source>ago</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">əvvəl</translation>
     </message>
 </context>
 <context>
@@ -50,49 +50,49 @@
         <location filename="../battery/upowerdevice.cpp" line="71"/>
         <source>%1 Battery</source>
         <comment>%1 is battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">% 1 Batareya</translation>
     </message>
     <message>
         <location filename="../battery/upowerdevice.cpp" line="87"/>
         <source>Lithium-ion</source>
         <comment>battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Lityum-ion</translation>
     </message>
     <message>
         <location filename="../battery/upowerdevice.cpp" line="89"/>
         <source>Lithium Polymer</source>
         <comment>battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Lityum Polimer</translation>
     </message>
     <message>
         <location filename="../battery/upowerdevice.cpp" line="91"/>
         <source>Lithium Iron Phosphate</source>
         <comment>battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Lityum Dəmir Fosfat</translation>
     </message>
     <message>
         <location filename="../battery/upowerdevice.cpp" line="93"/>
         <source>Lead Acid</source>
         <comment>battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Qurşun turşusu</translation>
     </message>
     <message>
         <location filename="../battery/upowerdevice.cpp" line="95"/>
         <source>Nickel Cadmium</source>
         <comment>battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nikel Kadmiyum</translation>
     </message>
     <message>
         <location filename="../battery/upowerdevice.cpp" line="97"/>
         <source>Nickel Metal Hydride</source>
         <comment>battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nikel Metal Hidrit</translation>
     </message>
     <message>
         <location filename="../battery/upowerdevice.cpp" line="99"/>
         <source>Unknown</source>
         <comment>battery technology</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Naməlum</translation>
     </message>
 </context>
 </TS>
